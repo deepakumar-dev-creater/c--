1f7eedd5ff0e199a36253ee9de789f073643547b
Deepak Kumar <sean4deepak@gmail.com>
2023-11-26 16:45:28 +0000

    done

--- Lamdha/src/main.cpp
+++ Lamdha/src/main.cpp
@@ -2,13 +2,23 @@
 using namespace std;
 int main ()
 {
+    int x =2;
+    auto greet  = [x] (int* value) {
+        ++value;
 
-    cout<<"Hello- World \n";
 
-    auto greet  = [] () {
-        cout<<"Hello";
     };
 
-    greet();
+    struct Person
+    {
+        int Id;
+        string name;
+    };
+
+    Person p = {1,"deepak"};
+
+    cout<<p.name;
+    
+    cout<<"THe value of a after change \n";
     return 0;
 }
